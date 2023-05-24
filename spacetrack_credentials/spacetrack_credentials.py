5c87fd81a670cee353dc6a611e75bdd5d7d903d0
Gregory Halverson <gregory.halverson@gmail.com>
2023-05-23 20:50:08 -0700

    fixing credential loading

--- spacetrack_credentials/spacetrack_credentials.py
+++ spacetrack_credentials/spacetrack_credentials.py
@@ -1,13 +1,18 @@
 from typing import Dict
-from os.path import join, abspath, dirname
+from os.path import join, abspath, dirname, exists
 from credentials import get_credentials
 
 FILENAME = join(abspath(dirname(__file__)), "spacetrack_credentials.txt")
 
 def get_spacetrack_credentials(filename: str = FILENAME) -> Dict[str, str]:
-    return get_credentials(
+    if filename is None or not exists(filename):
+        filename = FILENAME
+    
+    credentials = get_credentials(
         filename=filename,
         displayed=["username"],
         hidden=["password"],
         prompt="credentials for Spacetrack https://www.space-track.org/auth/createAccount"
     )
+
+    return credentials
