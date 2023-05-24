7ea2cf13f4ff32ae1bac261a3a81ae142f3d1e49
Gregory Halverson <gregory.halverson@gmail.com>
2023-05-23 20:49:57 -0700

    fixing credential loading

--- ERS_credentials/ERS_credentials.py
+++ ERS_credentials/ERS_credentials.py
@@ -1,13 +1,18 @@
 from typing import Dict
-from os.path import join, abspath, dirname
+from os.path import join, abspath, dirname, exists
 from credentials import get_credentials
 
 FILENAME = join(abspath(dirname(__file__)), "ERS_credentials.txt")
 
 def get_ERS_credentials(filename: str = FILENAME) -> Dict[str, str]:
-    return get_credentials(
+    if filename is None or not exists(filename):
+        filename = FILENAME
+
+    credentials = get_credentials(
         filename=filename,
         displayed=["username", "header"],
         hidden=["password"],
         prompt="credentials for EROS Registration System https://ers.cr.usgs.gov/register"
     )
+
+    return credentials
